aa84297503323b8b5a5bb3aa766a6c9623d7d5da
hara <hara.acharya@gmail.com>
2020-01-24 14:40:56 -0800

    fixed ssh server name issue for chrome test devices

--- speed_test.sh
+++ speed_test.sh
@@ -10,27 +10,29 @@ test_file="test.img"
 # Optional: user specified test file size in MBs or GBs
 if [ -z "$2" ]
 then
-  test_size=1
+  test_size=5G
 else
   test_size=$2
 fi
 
+`rm $test_file`
 echo "Generating $test_size"Bs" test file..."
 `fallocate -l $test_size test.img`
+echo "File generated"
 #or can use dd command as below
 # `dd if=/dev/zero of=$test_file bs=$(echo "$test_size*1000000" | bc) \
 #   count=1 &> /dev/null`
 
 # upload test
 echo "Testing upload speed to $ssh_server..."
-up_speed=`sshpass -p "test0000" scp -v $test_file $ssh_server:/home/chronos/$test_file 2>&1 | \
+up_speed=`sshpass -p "test0000" scp -v $test_file root@$ssh_server:/home/chronos/$test_file 2>&1 | \
   grep "Bytes per second" | \
   sed "s/^[^0-9]*\([0-9.]*\)[^0-9]*\([0-9.]*\).*$/\1/g"`
 up_speed=`echo "($up_speed*0.0009765625*100.0+0.5)/1*0.01" | bc`
 
 # download test
 echo "Testing download speed from $ssh_server..."
-down_speed=`sshpass -p "test0000" scp -v $ssh_server:$test_file /home/chronos/$test_file 2>&1 | \
+down_speed=`sshpass -p "test0000" scp -v $test_file root@$ssh_server:/home/chronos/$test_file 2>&1 | \
   grep "Bytes per second" | \
   sed "s/^[^0-9]*\([0-9.]*\)[^0-9]*\([0-9.]*\).*$/\2/g"`
 down_speed=`echo "($down_speed*0.0009765625*100.0+0.5)/1*0.01" | bc`
